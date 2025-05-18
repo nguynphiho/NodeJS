ca84fa5667001314ea3910bfa340aa8cf8fac89e
honp <nguyenphiho147@gmail.com>
2025-05-18 18:38:44 +0700

    [lesson9b] Create refresh token handler.

--- src/services/keytoken.service.js
+++ src/services/keytoken.service.js
@@ -23,11 +23,23 @@ class KeyTokenService {
     }
 
     static findByUserId = async (userId) => {
-        return keytokenModel.findOne({ user: new Types.ObjectId(userId)}).lean();
+        return await keytokenModel.findOne({ user: new Types.ObjectId(userId)}).lean();
     }
 
     static removeKeyById = async (id) => {
         return await keytokenModel.deleteOne(id);
+    }
+
+    static findByRefreshTokenUsed = async (refreshToken) => {
+        return await keytokenModel.findOne({ refreshTokensUsed: refreshToken }).lean()
+    }
+
+    static findByRefreshToken = async (refreshToken) => {
+        return await keytokenModel.findOne({ refreshToken })
+    }
+
+    static deleteKeyById = async (userId) => {
+        return await keytokenModel.deleteOne({ user: new Types.ObjectId(userId) })
     }
 }
 
